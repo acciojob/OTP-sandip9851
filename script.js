1105ac66ed24fee2777184f3ba7dd37fcd0f75b6
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-26 19:03:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let inputs = document.querySelectorAll('.code');
+inputs.forEach((input, index) => {
+    input.addEventListener('input', () => {
+        if(index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    });
+});
