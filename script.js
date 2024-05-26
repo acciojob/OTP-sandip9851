e4e8baf1cbd761d315f54406cb360d4661bdf61e
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-26 19:12:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-//your JS code here. If required.
 let inputs = document.querySelectorAll('.code');
 inputs.forEach((input, index) => {
     input.addEventListener('input', () => {
         if(index < inputs.length - 1) {
             inputs[index + 1].focus();
+        }
+    });
+    input.addEventListener('keydown', (e) => {
+        if(e.key === "Backspace" && input.value === "" && index > 0) {
+            inputs[index - 1].focus();
         }
     });
 });
